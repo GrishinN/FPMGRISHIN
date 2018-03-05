cf07633e6872091e56beb12625a9a7c35d97d13b
Nikita <grishinnikitaa@gmai.com>
2018-03-05 18:29:10 +0300

    add .js file

---
+++ JavaScript.js
@@ -0,0 +1,233 @@
+var photoPosts = [
+    {
+        id: '',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '2',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '3',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '4',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '5',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '6',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '7',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '8',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '9',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '10',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '11',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '12',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },{
+        id: '13',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '14',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },{
+        id: '15',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },{
+        id: '16',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '17',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '18',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '19',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '20',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    }
+];
+
+(function() {
+    function removePhotoPost(value) {
+
+        for (var i = 0; i < photoPosts.length; i++) {
+            if (photoPosts[i].id === value){
+                photoPosts.splice(i,1);
+                return 1;
+            }
+        }
+
+        return -1;
+    }
+    function getPhotoPost(value) { // 1й выриант
+        for (var i = 0; i < photoPosts.length; i++) {
+            if (photoPosts[i].id === value){
+
+                return photoPosts[i];
+            }
+        }
+
+        return -1;
+    }
+
+    function getPhotoPost(value) { // 2й вариант
+        a = photoPosts.findIndex(item => item.id === value);
+        if(a !== -1 )
+        {
+            return photoPosts[a];
+        }else{
+            return false;
+        }
+
+    }
+
+    function validatePhotoPost(photoPosts) {
+        if((typeof photoPosts.id !== "string") || photoPosts.id == '') {
+            return -1;
+        }
+        if((typeof photoPosts.descriprion !== "string") || photoPosts.descriprion == '' || photoPosts.descriprion.length > 200) {
+            return -1;
+        }
+        if((typeof photoPosts.createdAt !== "object" || photoPosts.createdAt == '') ) {
+            return -1;
+        }
+
+        if((typeof photoPosts.author !== "string") || photoPosts.author == '') {
+            return -1;
+        }
+        if((typeof photoPosts.photoLink !== "string") || photoPosts.photoLink == '') {
+            return -1;
+        }
+        return true;
+    }
+
+    function addPhotoPost(photoPosts1) {
+        if(validatePhotoPost(photoPosts1)){
+            if(photoPosts.push(photoPosts1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function  editPhotoPost(id , photoPosts1 ) {
+        var item = photoPosts.findIndex(item => item.id === id);
+        if(validatePhotoPost(photoPosts[item])){
+            for(var key in photoPosts1){
+                if(key == "descriprion"){
+                    photoPosts[item].descriprion = photoPosts1[key];
+                }
+                if(key == "createdAt"){
+                    photoPosts[item].createdAt = photoPosts1[key];
+                }
+                if(key == "photoLink"){
+                    photoPosts[item].photoLink = photoPosts1[key];
+                }
+            }
+        }
+    }
+
+    function getPhotoPosts(skip , top , filterConfig) {
+        if(filterConfig === undefined){
+
+        }
+
+    }
+
+
+
+
+
+
+})();
